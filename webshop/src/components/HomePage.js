0721d2abc41ae17a3b2612dcf5a3a562e3d1ac91
JohanBirger <67735706+JohanBirger@users.noreply.github.com>
2020-07-28 21:56:47 +0200

    Update HomePage.js

--- webshop/src/components/HomePage.js
+++ webshop/src/components/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import {Button} from './Button';
 import styled from 'styled-components'
+import {ProductWrapper} from './Product';
+import {*} from '../Context.js';
 
 export default function HomePage() {
     return (
@@ -18,36 +20,35 @@ export default function HomePage() {
                             {/* end title */}
                             {/* product info */}
     <div className="row">
-                                {/* Product img*/}
+                                
 
-        <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-              <img src={img} className="img-fluid" alt="product"/>
-        </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <ProductConsumer>
+                        {(value)=>( 
+                        <div className="img-container p-5" 
+                        onClick={()=>
+                            value.handleDetail(id)
+                        }>
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top"
+                            />
+                        </Link>
+    
+                    </div>)}
+                   
+                    </ProductConsumer>
+
+                </div>
+            </ProductWrapper>
 
        
-                                {/* Product text*/}
-        <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-            <h2>{/*title*/} </h2>
-            <h4 className="textTitle text-capitalize text-muted mt-3 mb-2">
-                Under construction <span className="text-capitalize"></span>
-            </h4>
-            <h4 className="textMainBlack"><strong> </strong>
-            </h4>
-            <p className="text-capitalize.font-weight-bold.mt-3.mb-0">
-                Come back soon for feed and socialmedia display!
-            </p>
-            <p className="text-muted lead"></p>
-            {/*buttons*/}
-            <div>
-                <Link to='/products'>
-                    <Button>
-                        Go to products
-                    </Button>
+                               
                                             
-                </Link>
+                
 
-            </div>
-        </div>
+            
+        
                                 
     </div>
 </div>
