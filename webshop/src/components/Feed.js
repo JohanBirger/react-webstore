dd5b5fb43062e6f0de4ca306c333bc7e22313398
JohanBirger <67735706+JohanBirger@users.noreply.github.com>
2020-07-28 22:50:41 +0200

    Update Feed.js

--- webshop/src/components/Feed.js
+++ webshop/src/components/Feed.js
@@ -17,7 +17,7 @@ export default class Product extends Component {
                 <div className="card">
                     <ProductConsumer>
                         {(value)=>( 
-                        <div className="img-container p-5" 
+                        <div className="img-container p-1" 
                         onClick={()=>
                             value.handleDetail(id)
                         }>
@@ -25,31 +25,12 @@ export default class Product extends Component {
                             <img src={img} alt="product" className="card-img-top"
                             />
                         </Link>
-                        <button className="cart-btn" 
-                        disabled={inCart ? true : false}
-                        onClick={()=>{
-                            value.addToCart(id);
-                            value.openModal(id);
-                        }}
-                        >
-                        {inCart ? (<p className="text-capitalize mb-0" disabled>
-                            in Cart</p>):(<i className="fa fa-cart-plus"/>)}
-                            </button>
+                      
                     </div>)}
                    
                     </ProductConsumer>
 
-                    {/*card footer */}
-                    <div className="card-footer d-flex justify-content-between">
-                        <p className="textMainBlack align-self-center mb-0 px-2">
-                            {title}
-                        </p>
-                        <h5 className="textMainBlack font-bold mb-0">
-                            <span className="mr-1">SEK</span>
-                            {price}
-                        </h5>
-
-                    </div>
+     
                 </div>
             </ProductWrapper>
         )
