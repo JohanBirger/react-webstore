61b64f00fa7bbe8894eb90392d4e3dbf8c41bfdb
JohanBirger <67735706+JohanBirger@users.noreply.github.com>
2020-07-28 22:37:14 +0200

    Update HomePage.js

--- webshop/src/components/HomePage.js
+++ webshop/src/components/HomePage.js
@@ -1,61 +1,38 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import {Button} from './Button';
-import styled from 'styled-components'
-import {ProductWrapper} from './Product';
-import {ProductConsumer} from '../context'
-import PropTypes from 'prop-types'
 
-export default function HomePage() {
-    return (
+import React, { Component } from 'react';
+import Product from './Product';
+import Title from './Title';
+import {ProductConsumer} from '../context';
+import { dom } from '@fortawesome/fontawesome-svg-core'
 
-        const {id, title, img, price, inCart} = this.props.product
+dom.watch()
 
-        <div>
-                    <div className="container py-5">
-                            {/* title */}
-    <div className="row">
-          <div className="col-10 mx-auto text center text-slanted textMainBlack my-5">
-            <h1>
-                Feed
-            </h1>
-        </div>
-    </div>
-                            {/* end title */}
-                            {/* product info */}
-    <div className="row">
-                                
 
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                <div className="card">
-                    <ProductConsumer>
-                        {(value)=>( 
-                        <div className="img-container p-5" 
-                        onClick={()=>
-                            value.handleDetail(id)
-                        }>
-                        <Link to="/details">
-                            <img src={img} alt="product" className="card-img-top"
-                            />
-                        </Link>
+export default class Productlist extends Component {
     
-                    </div>)}
-                   
-                    </ProductConsumer>
+    render() {
+        return (
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="Have a look at our" title="feed"/>
 
+                        <div className="row">
+                            <ProductConsumer>
+                                {value =>{
+                                    return value.products.map(product=>{
+                                        return <Product key={product.id} product={product}  />
+                                    })
+
+                                }}
+                            </ProductConsumer>
+                        </div>
+                    </div>
                 </div>
-            </ProductWrapper>
-
-       
-                               
-                                            
-                
-
+            </React.Fragment>
             
-        
-                                
-    </div>
-</div>
-        </div>
-    )
+               // 
+            
+        )
+    }
 }
